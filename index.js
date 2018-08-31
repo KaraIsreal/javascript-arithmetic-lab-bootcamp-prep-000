e407ad536c2943fbda0bc94d66b6d4c1c9c39e27
Kara Isreal <isreal.kara92@gmail.com>
2018-08-31 06:30:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-var number = 5
+function add(a, b) {
+  return (a+b)
+}
+function subtract(b, a) {
+  return (b-a)
+}
+function multiply(a, b) {
+  return (a*b)
+}
+function divide(a,b) {
+  return (a/b)
+}
+function inc(n) {
+  return inc++
+}
+function dec(n) {
+  return dec--
+}
+function makeInt(n) {
+  return parseInt(n)
+}
+
